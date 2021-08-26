38d8f6a8cdafad3dc67ad43d0d335129dddb848e
Steeve Fong <steeve.fong@gmail.com>
2021-08-26 09:28:17 +0400

    added readme

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# the-milky-way
-Nasa's image apis
+# The Milky Way
+
+## Approach
+
+- The project is built using MVVM architecture. 
+- RxSwift was used in favor of Combine because the project required using UIKit and storyboards. RxCocoa works great with UIKit and Combine is used to it's full potential when coding with SwiftUI.
+- Alamofire was used for faster network implementation.
+- SDWebImage was used to cache images and contained UIImageView extensions for faster implementation.
+
+## Improvements
+
+- More unit testing: Api calls, View Models, UI Tests, functions etc...
+- To add Internet connection listener
+- More error handlers, handle specific errors individually
+- Add bug reporting service
+- Add analytics
+- When app grow bigger, we can introduce a more modular structure together with MVVM
---
+++ the-milky-wayTests/ExtensionTests.swift
@@ -0,0 +1,27 @@
+//
+//  the_milky_wayTests.swift
+//  TheMilkyWayTests
+//
+//  Created by Steeve Fong on 2021/08/21.
+//
+
+import XCTest
+@testable import the_milky_way
+
+class TheMilkyWayTests: XCTestCase {
+    func testDisplayImageDateInProperFormat() throws {
+        let test = "2002-03-20T00:00:00Z".iso8601ToDate()
+        let test2 = "2009-12-21T00:00:00Z".iso8601ToDate()
+        
+        XCTAssertEqual(test!.displayImageDate(), "20 Mar, 2002")
+        XCTAssertEqual(test2!.displayImageDate(), "21 Dec, 2009")
+    }
+    
+    func testLocalizedExtensionn() {
+        let test = "noDataError".localized
+        let test2 = "unknownError".localized
+        
+        XCTAssertEqual(test, "It seems there is no data present for this search.")
+        XCTAssertEqual(test2, "An unknown error has occurred.")
+    }
+}
